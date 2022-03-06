1eb76d118b5629735b7e37f20abeea4cd9c6d283
Dustin Loring <dustinwloring1988@gmail.com>
2022-03-06 18:35:01 -0500

    Update setup-all-containers.sh

--- setup-all-containers.sh
+++ setup-all-containers.sh
@@ -1,1 +1,10 @@
-TODO: Create script that installs all containers in this repo
+echo "Spinning up all the containers this might take a while"
+dokcer-compose up -d -f /ddns/docker-compose.yml
+echo "Spun up ddns"
+dokcer-compose up -d -f /npm/docker-compose.yml
+echo "Spun up Nnginx Proxy Manager"
+dokcer-compose up -d -f /npm-goaccess/docker-compose.yml
+echo "Spun up Nnginx Proxy Manager - GoAccess"
+dokcer-compose up -d -f /f2b/docker-compose.yml
+echo "Spun up Fail 2 Ban"
+echo "All done enjoy"
